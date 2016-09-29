28fe8711a3004bcdc45b59b7e5eec8b2c5ee64fe
Adrian S MUresan <adriansmuresan@gmail.com>
2016-09-28 21:00:33 -0500

    A comment can be deleted

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy #if a post gets deleted, then comments get deleted as well
+  
   validates :title, presence: true, length: { minimum: 5}
   validates :body, presence: true
 end
